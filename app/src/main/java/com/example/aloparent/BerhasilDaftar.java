b188dd17cdf87384642672ad1102b94952dbdf82
GandiYahya <gandiyahya42@gmail.com>
2022-05-29 19:27:20 +0700

    screen home 1 selesai

--- app/src/main/java/com/example/aloparent/BerhasilDaftar.java
+++ app/src/main/java/com/example/aloparent/BerhasilDaftar.java
@@ -2,9 +2,16 @@ package com.example.aloparent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class BerhasilDaftar extends AppCompatActivity {
+
+    public void toLogin2(View v){
+        Intent intent = new Intent(BerhasilDaftar.this, LoginScreen.class);
+        startActivity(intent);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/com/example/aloparent/BerhasilLogin.java
+++ app/src/main/java/com/example/aloparent/BerhasilLogin.java
@@ -2,9 +2,16 @@ package com.example.aloparent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class BerhasilLogin extends AppCompatActivity {
+
+    public void toHome1(View v){
+        Intent intent = new Intent(BerhasilLogin.this, Home.class);
+        startActivity(intent);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
