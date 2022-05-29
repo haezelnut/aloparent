bf90ac48c8bff5b65b831dd2adcf84b0a138a002
Arsenius Purbandono <xzars@upi.edu>
2022-05-29 17:46:11 +0700

    Take Photo, Video Player

--- app/src/main/java/com/example/aloparent/DataAnak.java
+++ app/src/main/java/com/example/aloparent/DataAnak.java
@@ -4,11 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.aloparent.databinding.ActivityDataAnakBinding;
+
 public class DataAnak extends AppCompatActivity {
+
+    ActivityDataAnakBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_data_anak);
+        binding = ActivityDataAnakBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
     }
 }
--- app/src/main/java/com/example/aloparent/UpdateDataAnak.java
+++ app/src/main/java/com/example/aloparent/UpdateDataAnak.java
@@ -14,7 +14,6 @@ import android.widget.Button;
 import android.widget.DatePicker;
 
 import com.github.dhaval2404.imagepicker.ImagePicker;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.Calendar;
 
