25a8f33d32d0928ac4905cf2b144fe70bddca57c
Arsenius Purbandono <xzars@upi.edu>
2022-07-21 12:25:02 +0700

    Artikel

--- app/src/main/java/com/example/aloparent/View/Artikel.java
+++ app/src/main/java/com/example/aloparent/View/Artikel.java
@@ -1,6 +1,7 @@
 package com.example.aloparent.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -14,27 +15,54 @@ import com.example.aloparent.R;
 
 public class Artikel extends AppCompatActivity {
 
+    private CardView artikel1,artikel2,artikel3,artikel4;
+
     public void backFromArtikel(View v){
         Intent intent = new Intent(Artikel.this, Home.class);
         startActivity(intent);
-    }
-
-    public void artikel1(View v){
-        Intent intent = new Intent(Artikel.this, Artikel1.class);
-    }
-    public void artikel2(View v){
-        Intent intent = new Intent(Artikel.this, Artikel2.class);
-    }
-    public void artikel3(View v){
-        Intent intent = new Intent(Artikel.this, Artikel3.class);
-    }
-    public void artikel4(View v){
-        Intent intent = new Intent(Artikel.this, Artikel4.class);
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_artikel);
+
+        artikel1 = findViewById(R.id.artikel1);
+        artikel2 = findViewById(R.id.artikel2);
+        artikel3 = findViewById(R.id.artikel3);
+        artikel4 = findViewById(R.id.artikel4);
+
+        artikel1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Artikel.this, Artikel1.class);
+                startActivity(intent);
+            }
+        });
+
+        artikel2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Artikel.this, Artikel2.class);
+                startActivity(intent);
+            }
+        });
+
+        artikel3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Artikel.this, Artikel3.class);
+                startActivity(intent);
+            }
+        });
+
+        artikel4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Artikel.this, Artikel4.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
